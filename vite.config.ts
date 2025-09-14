78114ee52caae879a9e962d5b075eedf2db0d11c
corneliu756 <162913180+ItsWaferz@users.noreply.github.com>
2025-09-14 18:16:37 +0300

    Custom DNS fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/astral-events",
+  // IMPORTANT: pentru domeniu custom site-ul e servit din rădăcină:
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
